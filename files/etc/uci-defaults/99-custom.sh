1dd456886de296ac26c495a2c6fbf364a90bfded
Jack <55940640+jjqper@users.noreply.github.com>
2025-01-18 13:18:18 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -65,7 +65,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by Jack"
+NEW_DESCRIPTION="ImmortalWrt 23.05.4"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
